e42dcceda21f99ae4571943332d4c68b95aed5b5
ThanhPhan <pvthanh98@gmail.com>
2022-06-16 18:53:06 +0700

    Add Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { SharedModulesModule } from './modules/shared_modules/shared_modules.mod
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [
@@ -14,6 +16,18 @@ import { AuthModule } from './modules/auth/auth.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..','public'),
       serveRoot: "/app/",
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'ec2-52-71-23-11.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'jfxwylyhvkbpng',
+      password: '39e28d78ef50d58a8e2ad4321c9517df9ac8ff3d519b73e314ec4a02ecfcc23a',
+      database: 'd86adq6eek3nqm',
+      entities: [
+        User
+      ],
+      synchronize: true,
     }),
     DashboardModule,
     SharedModulesModule,
